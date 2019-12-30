8451b028076cd14d2dc7f809c4dc7a407c09999b
Hamed <hamedrazor54@gmail.com>
2019-12-30 16:07:15 +0330

    test commit for learn git for aspkar <3

--- ml_map_functions/src/main/java/ir/clinicemashin/ml_map_functions/MehrdadLatifiMap.java
+++ ml_map_functions/src/main/java/ir/clinicemashin/ml_map_functions/MehrdadLatifiMap.java
@@ -1,6 +1,7 @@
 package ir.clinicemashin.ml_map_functions;
 
 import android.location.Location;
+
 import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -29,53 +30,29 @@ import java.util.List;
 
 public class MehrdadLatifiMap {
 
-
-
-    public static void MlMap_DrawPolylines(
-            GoogleMap googleMap,
-            List<LatLng> latLngList,
-            int StrokeWidth,
-            int StrokeColor) {//____________________________________________________________________ Start DrawPolylines
+    public static void MlMap_DrawPolylines(GoogleMap googleMap, List<LatLng> latLngList, int StrokeWidth, int StrokeColor) {
         Polyline polyline = googleMap.addPolyline(new PolylineOptions()
                 .clickable(true)
                 .addAll(latLngList));
-        stylePolyline(polyline,StrokeWidth,StrokeColor);
-    }//_____________________________________________________________________________________________ End DrawPolylines
+        stylePolyline(polyline, StrokeWidth, StrokeColor);
+    }
 
-
-    private static void stylePolyline(Polyline polyline, int StrokeWidth, int StrokeColor) {//_____________ Start stylePolyline
+    private static void stylePolyline(Polyline polyline, int StrokeWidth, int StrokeColor) {
         polyline.setStartCap(new RoundCap());
         polyline.setEndCap(new RoundCap());
         polyline.setWidth(StrokeWidth);
         polyline.setColor(StrokeColor);
         polyline.setJointType(JointType.ROUND);
+    }
 
-    }//_____________________________________________________________________________________________ End stylePolyline
-
-
-    public static void MlMap_DrawPolygon(
-            GoogleMap googleMap,
-            List<LatLng> latLngList,
-            int StrokeWidth,
-            int StrokeColor,
-            int FillColor) {//______________________________________________________________________ Start DrawPolylines
+    public static void MlMap_DrawPolygon(GoogleMap googleMap, List<LatLng> latLngList, int StrokeWidth, int StrokeColor, int FillColor) {
         Polygon polygon = googleMap.addPolygon(new PolygonOptions()
                 .clickable(true)
                 .addAll(latLngList));
-        stylePolygon(polygon, false, StrokeWidth,StrokeColor,FillColor,0,0);
-    }//_____________________________________________________________________________________________ End DrawPolylines
+        stylePolygon(polygon, false, StrokeWidth, StrokeColor, FillColor, 0, 0);
+    }
 
-
-
-    private static void stylePolygon(
-            Polygon polygon,
-            boolean Pattern,
-            int StrokeWidth,
-            int StrokeColor,
-            int FillColor,
-            int PatternDashLenghtPx,
-            int PatternGapLenghtPx) {//_____________________________________________________________ Start stylePolygon
-
+    private static void stylePolygon(Polygon polygon, boolean Pattern, int StrokeWidth, int StrokeColor, int FillColor, int PatternDashLenghtPx, int PatternGapLenghtPx) {
         if (Pattern) {
             PatternItem DASH = new Dash(PatternDashLenghtPx);
             PatternItem GAP = new Gap(PatternGapLenghtPx);
@@ -86,58 +63,37 @@ public class MehrdadLatifiMap {
         polygon.setStrokeWidth(StrokeWidth);
         polygon.setStrokeColor(StrokeColor);
         polygon.setFillColor(FillColor);
-    }//_____________________________________________________________________________________________ End stylePolygon
+    }
 
-
-
-
-    public static void MlMap_DrawCircle(
-            GoogleMap googleMap,
-            LatLng latLng,
-            double radius,
-            int StrokeWidth,
-            int StrokeColor,
-            int FillColor) {//______________________________________________________________________ Start DrawCircle
+    public static void MlMap_DrawCircle(GoogleMap googleMap, LatLng latLng, double radius, int StrokeWidth, int StrokeColor, int FillColor) {
         Circle circle = googleMap.addCircle(new CircleOptions()
                 .center(latLng)
                 .radius(radius)
                 .strokeWidth(StrokeWidth)
                 .strokeColor(StrokeColor)
                 .fillColor(FillColor));
-    }//_____________________________________________________________________________________________ End DrawCircle
+    }
 
-
-
-    public static float[] MlMap_MeasureDistance(LatLng Old, LatLng New) {//______________________________________ Start MeasureDistance
+    public static float[] MlMap_MeasureDistance(LatLng OldLatLng, LatLng NewLatLng) {
         float[] results = new float[1];
-        Location.distanceBetween(Old.latitude, Old.longitude,
-                New.latitude, New.longitude, results);
+        Location.distanceBetween(OldLatLng.latitude, OldLatLng.longitude, NewLatLng.latitude, NewLatLng.longitude, results);
         return results;
-    }//_____________________________________________________________________________________________ End MeasureDistance
+    }
 
-
-    public static Marker MlMap_AddMarker(
-            GoogleMap googleMap,
-            LatLng latLng,
-            String title,
-            String tag,
-            int icon) {//___________________________________________________________________________ Start AddMarker
+    public static Marker MlMap_AddMarker(GoogleMap googleMap, LatLng latLng, String title, String tag, int icon) {
         Marker marker = googleMap.addMarker(new MarkerOptions()
                 .position(latLng)
                 .title(title)
                 .icon(BitmapDescriptorFactory.fromResource(icon)));
         marker.setTag(tag);
         return marker;
-    }//_____________________________________________________________________________________________ End AddMarker
+    }
 
-
-    public static Boolean MlMap_isInside(LatLng point, List<LatLng> latLngs) {//_________________________________ Start isInside
+    public static Boolean MlMap_isInside(LatLng point, List<LatLng> latLngs) {
         return PolyUtil.containsLocation(point, latLngs, true);
-    }//_____________________________________________________________________________________________ End isInside
+    }
 
-
-
-    public static ArrayList<LatLng> MlMap_getCirclePoints(LatLng centre, double radius) {//_________ Start getCirclePoints
+    public static ArrayList<LatLng> MlMap_getCirclePoints(LatLng centre, double radius) {
         ArrayList<LatLng> points = new ArrayList<LatLng>();
 
         double EARTH_RADIUS = 6378100.0;
@@ -149,14 +105,12 @@ public class MehrdadLatifiMap {
             double lonPoint = lon + (radius / EARTH_RADIUS) * Math.cos(t) / Math.cos(lat);
             LatLng point = new LatLng(latPoint * 180.0 / Math.PI, lonPoint * 180.0 / Math.PI);
             points.add(point);
-
         }
 
         return points;
-    }//_____________________________________________________________________________________________ End getCirclePoints
+    }
 
-
-    public static void MlMap_AutoZoom(GoogleMap googleMap,List<LatLng> latLngs) {//_________________ Start AutoZoom
+    public static void MlMap_AutoZoom(GoogleMap googleMap, List<LatLng> latLngs) {
         LatLngBounds.Builder builder = new LatLngBounds.Builder();
         for (LatLng latLng : latLngs) {
             builder.include(latLng);
@@ -165,12 +119,11 @@ public class MehrdadLatifiMap {
         int padding = 0; // offset from edges of the map in pixels
         CameraUpdate cu = CameraUpdateFactory.newLatLngBounds(bounds, padding);
         googleMap.animateCamera(cu);
-    }//_____________________________________________________________________________________________ End AutoZoom
+    }
 
 
-
-//    public Location getCurrentLocation(FragmentActivity context) {//___________________________________ Start getDeviceLocation
-//        LocationRequest request = LocationRequest.create() //standard GMS LocationRequest
+//    public Location getCurrentLocation(FragmentActivity context) {
+//        LocationRequest request = LocationRequest.create()
 //                .setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY)
 //                .setNumUpdates(1)
 //                .setInterval(1000);
@@ -184,10 +137,9 @@ public class MehrdadLatifiMap {
 //                    }
 //                });
 //
-//    }//_____________________________________________________________________________________________ End getDeviceLocation
-//
-//
-//    public void findAddress(Context c, String location, PublishSubject<String> Observables) {//_____ Start findAddress
+//    }
+
+//    public void findAddress(Context c, String location, PublishSubject<String> Observables) {
 //        if (location != null && !location.equalsIgnoreCase("")) {
 //            Locale locale = new Locale("fa_IR");
 //            Locale.setDefault(locale);
@@ -207,7 +159,6 @@ public class MehrdadLatifiMap {
 //                Observables.onNext("NoAddress");
 //            }
 //        }
-//    }//_____________________________________________________________________________________________ End findAddress
-
+//    }
 
 }
